de784943b7331adba472fe9e3bcdc5a76729aa8c
Saara Lehtinen <saaralehtinen77@gmail.com>
2024-02-04 11:56:03 +0200

    deleting some comments

--- src/components/services/urlShorteningService.ts
+++ src/components/services/urlShorteningService.ts
@@ -17,21 +17,4 @@ export const fetchShortenLink = async (longUrl: string) => {
       console.error(error);
     }
   }
-
-  /* const response = await fetch("https://api-ssl.bitly.com/v4/shorten", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      Authorization: `Bearer ${process.env.REACT_APP_BITLY_TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      long_url: longUrl,
-      domain: "bit.ly",
-      group_guid: `${process.env.REACT_APP_GUID}`,
-    }),
-  });
-
-  const linkInfo = await response.json();
-  return linkInfo.link; */
 };
